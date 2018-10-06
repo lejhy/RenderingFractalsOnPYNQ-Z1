26ae91db90ebb09b6495ce45d2fa0316c701cd58
Filip Lejhanec <filip.lejhanec.2015@uni.strath.ac.uk>
2018-10-06 15:49:09 +0100

    Rough project setup with MVC

---
+++ src/model/Fractal.py
@@ -0,0 +1,8 @@
+import numpy
+
+class Fractal:
+    def calculate(self, x1, y1, x2, y2):
+        for x in range(x1, x2):
+            for y in range(y1, y2):
+                """calculate pixels"""
+
