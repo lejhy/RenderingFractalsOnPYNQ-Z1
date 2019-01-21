f1471793de1983ff0c4937a3b324276cb369d807
Filip Lejhanec <filip.lejhanec.2015@uni.strath.ac.uk>
2019-01-21 02:40:58 +0000

    Work in progress on optimizing IP

--- hls/source/Mandelbrot.cpp
+++ hls/source/Mandelbrot.cpp
@@ -1,10 +1,10 @@
 #include "Mandelbrot.h"
 
 void mandelbrot(AXI_STREAM& OUTPUT_STREAM, Config& config) {
-#pragma HLS INTERFACE s_axilite port=config
-#pragma HLS INTERFACE axis port=OUTPUT_STREAM
-#pragma HLS INTERFACE s_axilite port=return
-#pragma HLS DATAFLOW
+	#pragma HLS INTERFACE s_axilite port=config
+	#pragma HLS INTERFACE axis port=OUTPUT_STREAM
+	#pragma HLS INTERFACE s_axilite port=return
+	#pragma HLS DATAFLOW
 
 	RGB_IMAGE img(config.img_height, config.img_width);
 
@@ -16,22 +16,40 @@ void mandelbrot(AXI_STREAM& OUTPUT_STREAM, Config& config) {
 }
 
 void calculate(Config& config, RGB_IMAGE& img) {
+	RGB_PIXEL pixel_buffer[PARALLEL_LOOPS];
+	#pragma HLS array_partition variable=pixel_buffer complete
+
+
+
 	// image coordinates have origin at top left, plot coordinates have origin at bottom left
-	for(int img_y = 0; img_y < 1080; img_y++) {
-		for(int img_x = 0; img_x < 1920; img_x++) {
-			double plot_x = (double)img_x / (double)config.img_width * config.plot_width + config.plot_x_min;
-			double plot_y = config.plot_y_max - (double)img_y / (double)config.img_height * config.plot_height;
-			double x = 0.0;
-			double y = 0.0;
-			int iteration = 0;
-			while (x*x + y*y < 4 && iteration < config.max_iteration) {
-				double temp = x*x - y*y + plot_x;
-				y = 2*x*y + plot_y;
-				x = temp;
-				iteration = iteration + 1;
+	for(int img_y = 0; img_y < config.img_height; img_y++) {
+		for(int img_x = 0; img_x < config.img_width; img_x = img_x + PARALLEL_LOOPS) {
+			batchProcess(pixel_buffer, img_x, img_y, config);
+			for(int i = 0; i < PARALLEL_LOOPS; i++){
+				img.write(pixel_buffer[i]);
 			}
-			img.write(getPixel(iteration, config.max_iteration));
 		}
+	}
+}
+
+void batchProcess(RGB_PIXEL pixel_buffer[PARALLEL_LOOPS], int img_x, int img_y, Config config){
+	#pragma HLS INLINE region
+	#pragma HLS DATAFLOW
+
+	for(int i = 0; i < PARALLEL_LOOPS; i++){
+		#pragma HLS UNROLL
+		float plot_x = (float)(img_x + i) / (float)config.img_width * config.plot_width + config.plot_x_min;
+		float plot_y = config.plot_y_max - img_y / (float)config.img_height * config.plot_height;
+		float x = 0.0;
+		float y = 0.0;
+		int iteration = 0;
+		while (x*x + y*y < 4 && iteration < config.max_iteration) {
+			float temp = x*x - y*y + plot_x;
+			y = 2*x*y + plot_y;
+			x = temp;
+			iteration = iteration + 1;
+		}
+		pixel_buffer[i] = getPixel(iteration, config.max_iteration);
 	}
 }
 
--- hls/source/Mandelbrot.h
+++ hls/source/Mandelbrot.h
@@ -4,12 +4,15 @@
 #include "Config.h"
 #include "hls_video.h"
 
+#define PARALLEL_LOOPS 8
+
 typedef hls::stream<ap_axiu<32,1,1,1> >     AXI_STREAM;
 typedef hls::Scalar<3, unsigned char>       RGB_PIXEL;
 typedef hls::Mat<1080, 1920, HLS_8UC3>      RGB_IMAGE;
 
 void mandelbrot(AXI_STREAM& OUTPUT_STREAM, Config& config);
 void calculate(Config& config, RGB_IMAGE& img);
+void batchProcess(RGB_PIXEL pixel_buffer[PARALLEL_LOOPS], int img_x, int img_y, Config config);
 RGB_PIXEL getPixel(int& iteration, int& max_iteration);
 
 #endif
