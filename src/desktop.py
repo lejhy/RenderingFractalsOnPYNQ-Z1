a10b88d851f2152b4273e15bdc999f0c071457a1
Filip Lejhanec <filip.lejhanec.2015@uni.strath.ac.uk>
2019-02-09 00:56:25 +0000

    Fixed module names

--- src/desktop.py
+++ src/desktop.py
@@ -1,12 +1,12 @@
 import time
 
-from model.Mandelbrot import Mandelbrot
+from model.Mandelbrot_SW import Mandelbrot
 from view.MatplotlibView import MatplotlibView
-from controller.DesktopController import DesktopController
+from controller.Controller import Controller
 
 model = Mandelbrot()
 view = MatplotlibView(1920, 1080)
-controller = DesktopController(model, view)
+controller = Controller(model, view)
 
 start: float = time.time()
 controller.update(x_scale=[-2.5, 1], y_scale=[-1, 1], max_iteration=1000)
