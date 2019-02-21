9995a2e2d49e189f6178fd68cea060c726c8e633
Filip Lejhanec <filip.lejhanec.2015@uni.strath.ac.uk>
2019-02-21 17:41:57 +0000

    Extracted script for HDMI setup on PYNQ

---
+++ python/scripts/HDMI.py
@@ -0,0 +1,17 @@
+import Mandelbrot
+import AxiVDMA_Custom
+
+from pynq import Overlay
+
+overlay = Overlay('/home/xilinx/pynq/overlays/personal/Mandelbrot/Mandelbrot.bit')
+mandelbrot = overlay.video.mandelbrot
+vdma = overlay.video.axi_vdma
+hdmi_out = overlay.video.hdmi_out
+
+hdmi_out._vdma = vdma #Find out where is this coming from, possibly the class override?
+
+vdma.readchannel.mode = common.VideoMode(1920, 1080, 32)
+hdmi_out.configure(common.VideoMode(1920, 1080, 32))
+vdma.readchannel.start()
+hdmi_out.start()
+vdma.readchannel.tie(vdma.writechannel)
